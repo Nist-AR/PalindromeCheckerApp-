fe4c92e681889f70299e265de06437862e13121f
Nist-AR <catchmebasith27@gmail.com>
2026-02-27 15:45:01 +0530

    UC1: Add welcome message and application details

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,22 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<module type="JAVA_MODULE" version="4">
-  <component name="NewModuleRootManager" inherit-compiler-output="true">
-    <exclude-output />
-    <content url="file://$MODULE_DIR$">
-      <sourceFolder url="file://$MODULE_DIR$/src" isTestSource="false" />
-    </content>
-    <orderEntry type="inheritedJdk" />
-    <orderEntry type="sourceFolder" forTests="false" />
-  </component>
-</module>
+public class PalindromeCheckerApp {
+
+  public static void main(String[] args) {
+
+    // Application Name
+    System.out.println("=================================");
+    System.out.println("     PALINDROME CHECKER APP");
+    System.out.println("=================================");
+
+    // Application Version
+    System.out.println("Version: 1.0");
+    System.out.println("Author: Nist-AR");
+
+    // Welcome Message
+    System.out.println();
+    System.out.println("Welcome to the Palindrome Checker Application!");
+    System.out.println("This program will check whether a given string is a palindrome.");
+    System.out.println();
+
+    System.out.println("Application started successfully...");
+  }
+}
